6636a40f6e9b97e72778cb8f4af656ca40196b80
Shailesh J <shailesh21319@gmail.com>
2024-12-04 20:42:25 +0530

    Create 1st Madlibs.py
    In this project just a simple type of project create for beginners in Python Languages.

---
+++ 12 Project/1st Madlibs.py
@@ -0,0 +1,19 @@
+# string concatenation (aka how to put strings together)
+# suppose we want to create a storing that says (Subscribes to .............)
+# youtuber = "Technology Secret"  #Some string varaible
+
+
+# # a few ways to do this 
+# print("subscribe to "+ youtuber)
+# print("subsccribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjustive:")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+
+famous_person= input("Famous Person:")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!!"
+
+print(madlib)
